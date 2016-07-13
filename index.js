84b748c946bdce7b386686d27a3bbad5437cb585
Sameer Khoja <sameerkhoja10@gmail.com>
2016-07-13 15:39:22 -0500

    added process env

--- index.js
+++ index.js
@@ -88,6 +88,6 @@ app.get('/', function (req, res) {
   res.render('welcome');
 });
 
-app.listen(3000);
-
-console.log('Express server started on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
