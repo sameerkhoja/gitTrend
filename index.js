ede1a7fa6814264c245149055f9ff75c57f39416
Sameer Khoja <sk2327@cornell.edu>
2016-07-20 16:58:15 -0500

    update

--- index.js
+++ index.js
@@ -78,15 +78,13 @@ app.get('/tutors', function(req, res){
 
 });
 
-
-
-
 app.use(express.static(__dirname + '/views'));
 
 
 app.get('/', function (req, res) {
   res.render('welcome');
 });
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
